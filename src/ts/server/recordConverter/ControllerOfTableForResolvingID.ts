18bb55c991a4f8c9458e13b31bc7f2a4b27bb6fe
Appbird <project.appbird@gmail.com>
2021-04-12 19:48:30 +0900

    [refactoring] ControllerOfTableForResolvingIDのコードのリファクタリング。
    同じ操作をする部を分離して関数化したため、より保守性を高まった。

--- src/ts/server/recordConverter/ControllerOfTableForResolvingID.ts
+++ src/ts/server/recordConverter/ControllerOfTableForResolvingID.ts
@@ -6,96 +6,71 @@ import { InterfaceOfRecordDatabase } from "../type/InterfaceOfRecordDatabase";
 /**
  * データベースのデータを参照してIDを解決してくれるテーブルマネージャー
  */
-type ResolveTables = Map<string,IItemOfResolveTableToName[]>
+type ResolveTable = Map<string,IItemOfResolveTableToName>
 export class ControllerOfTableForResolvingID{
     
-    private gameSystem: IItemOfResolveTableToName[] = [];
-    private runners:IItemOfResolveTableToName[] = [];
+    //#NOTE キャッシュ。既に得たID-名前対応表を保存しておくことでFirestoreへの読み出しリクエスト回数を制限する。
+    private gameSystem:ResolveTable     = new Map<string,IItemOfResolveTableToName>();
+    private runner:ResolveTable         = new Map<string,IItemOfResolveTableToName>();
+    private hashTag:ResolveTable        = new Map<string,IItemOfResolveTableToName>();
+    private gameMode:ResolveTable       = new Map<string,IItemOfResolveTableToName>();
+    private difficulty:ResolveTable     = new Map<string,IItemOfResolveTableToName>();
+    private ability:ResolveTable        = new Map<string,IItemOfResolveTableToName>();
+    private target:ResolveTable         = new Map<string,IItemOfResolveTableToName>();
 
-    private gameMode:   ResolveTables = new Map<string,IItemOfResolveTableToName[]>();
-
-    private difficulties:ResolveTables = new Map<string,IItemOfResolveTableToName[]>();
-    private abilities:ResolveTables = new Map<string,IItemOfResolveTableToName[]>();
-    private targets:ResolveTables = new Map<string,IItemOfResolveTableToName[]>();
-    private tagIDs:ResolveTables = new Map<string,IItemOfResolveTableToName[]>();
     private database:InterfaceOfRecordDatabase;
 
-    private initalized:boolean = false;
-    private initErorMsg:string = "初期化がなされていません。まずinit()メソッドを実行してください。(ControllerOfTableForResolvingID)";
     //#NOTE コンストラクター・インジェクションの形を取ったので、モック化に対応できる。
     constructor(database:InterfaceOfRecordDatabase){
         this.database = database
 
     }
-    private checkInitalized(){
-        if (!this.initalized) throw new Error(this.initErorMsg)
-    }
-    async init(gameSystemID:string,gameModeID:string){
-        const env = `${gameSystemID}/${gameModeID}`;
-        this.gameSystem = await this.database.getGameSystemCollection()
-        this.runners    = await this.database.getRunnerCollection()
-        
-        this.gameMode.set(gameSystemID,(await this.database.getGameModeCollection(gameSystemID)))
-        this.targets        .set(gameSystemID,await this.database.getTargetCollection(gameSystemID,gameModeID))
 
-        this.difficulties   .set(env,await this.database.getGameDifficultyCollection(gameSystemID,gameModeID)  )
-        this.abilities      .set(env,await this.database.getAbilityCollection(gameSystemID,gameModeID)         )
-        this.initalized = true;
-        
-    }
-    private async resolveID(id:string, table:IItemOfResolveTableToName[] | undefined,lang:LanguageInApplication,descriptionOfPlace:string = ""){
-        if (table === undefined) throw new Error("idと名前の対応づけに失敗しました。")
-        const item = table.find(
-            (element) =>  element.id === id
-        )
-        if (item === undefined) throw new Error(`${descriptionOfPlace}テーブルにおいて、id${id}に対応するデータが存在しません。`);
-        switch(lang){
-            case "Japanese": return item.JName;
-            case "English": return item.EName;
+    private async selectAppropriateName(item:IItemOfResolveTableToName,lang:LanguageInApplication){
+        switch (lang){
+            case "Japanese": return item.JName
+            case "English": return item.EName
         }
     }
-    //#NOTE ここのメソッドについて、読み込む際に不足分があれば別途読み込む。
+    // #NOTE 基礎メソッド
+    private async getName(id:string, lang:LanguageInApplication, cacheList:ResolveTable,
+        getDoc:(id:string) => Promise<IItemOfResolveTableToName>
+    ){
+        const result = (this.hashTag.has(id)) ? await cacheList.get(id) : cacheList.set(id,await getDoc(id) ).get(id)
+        if (result === undefined) throw new Error("予期しないエラーです。")
+        return this.selectAppropriateName(result,lang)
+    }
+    private async getNameBySID(gameSystemID:string,id:string,lang:LanguageInApplication,
+        cacheList:ResolveTable,
+        getDoc:(gameSystemID:string,id:string) => Promise<IItemOfResolveTableToName>
+    ){
+        const accessKey = `${gameSystemID}/${id}`
+        const result = (this.hashTag.has(accessKey)) ? await cacheList.get(accessKey) : cacheList.set(accessKey,await getDoc(gameSystemID,id) ).get(accessKey)
+        if (result === undefined) throw new Error("予期しないエラーです。")
+        return this.selectAppropriateName(result,lang)
+    }
+    private async getNameBySIDMID(gameSystemID:string,gameModeID:string,id:string,lang:LanguageInApplication,
+        cacheList:ResolveTable,
+        getDoc:(gameSystemID:string,gameModeID:string,id:string) => Promise<IItemOfResolveTableToName>
+    ){
+        const accessKey = `${gameSystemID}/${gameModeID}/${id}`
+        const result = (this.hashTag.has(accessKey)) ? await cacheList.get(accessKey) : cacheList.set(accessKey,await getDoc(gameSystemID,gameModeID,id) ).get(accessKey)
+        if (result === undefined) throw new Error("予期しないエラーです。")
+        return this.selectAppropriateName(result,lang)
+    }
 
-    async resolveGameSystemID(id:string,lang:LanguageInApplication){
-        return this.resolveID(id,this.gameSystem,lang,"GameSystem");
-    }
-    async resolveRunnerID(id:string,lang:LanguageInApplication){
-        return this.resolveID(id,this.runners,lang,"runnersTable");
-    }
-
-
-    async resolveGameModeID(gameSystemID:string,id:string,lang:LanguageInApplication){
-        this.checkInitalized();
-        if (!this.gameMode.has(gameSystemID)) this.gameMode.set(gameSystemID,await this.database.getGameModeCollection(gameSystemID) )
-        return this.resolveID(id,this.gameMode.get(`${gameSystemID}`),lang,"GameMode");
-    }
-    async resolveTagID(gameSystemID: string, gameModeID: string, id: string, lang: LanguageInApplication) {
-        this.checkInitalized();
-        if (!this.tagIDs.has(`${gameSystemID}`)) this.targets.set(`${gameSystemID}`,await this.database.getHashTagCollection(gameSystemID) )
-        return this.resolveID(id,this.tagIDs.get(`${gameSystemID}/${gameModeID}`),lang,"Tags");
-    }
+    //#NOTE 応用メソッド
+    resolveGameSystemID = (id:string,lang:LanguageInApplication) =>  this.getName(id,lang,this.gameSystem,this.database.getGameSystemInfo)
+    resolveRunnerID     = (id:string,lang:LanguageInApplication) => this.getName(id,lang,this.runner,this.database.getRunnerInfo)
+    resolveGameModeID   = (gameSystemID:string, id:string, lang:LanguageInApplication) => this.getNameBySID(gameSystemID,id,lang,this.gameMode,this.database.getGameModeInfo)
+    resolveTagID        = (gameSystemID: string, id: string, lang: LanguageInApplication) => this.getNameBySID(gameSystemID,id,lang,this.hashTag,this.database.getHashTagInfo)
+    resolveAbilityID    = (gameSystemID:string, gameModeID:string, id:string, lang:LanguageInApplication) => this.getNameBySIDMID(gameSystemID,gameModeID,id,lang,this.ability,this.database.getAbilityInfo)
+    resolveTargetID     = (gameSystemID:string, gameModeID:string, id:string, lang:LanguageInApplication) => this.getNameBySIDMID(gameSystemID,gameModeID,id,lang,this.target,this.database.getTargetInfo)
+    resolveGameDifficultyID = (gameSystemID:string, gameModeID:string, id:string, lang:LanguageInApplication) => this.getNameBySIDMID(gameSystemID,gameModeID,id,lang,this.difficulty,this.database.getGameDifficultyInfo)
     
-
-    async resolveAbilityID(gameSystemID:string,gameModeID:string,id:string,lang:LanguageInApplication){
-        this.checkInitalized();
-        if (!this.abilities.has(`${gameSystemID}/${gameModeID}`)) this.abilities.set(`${gameSystemID}/${gameModeID}`,await this.database.getAbilityCollection(gameSystemID,gameModeID) )
-        return this.resolveID(id,this.abilities.get(`${gameSystemID}/${gameModeID}`),lang,"Ability");
-    }
-    async resolveTargetID(gameSystemID:string,gameModeID:string,id:string,lang:LanguageInApplication){
-        this.checkInitalized();
-        if (!this.targets.has(`${gameSystemID}/${gameModeID}`)) this.targets.set(`${gameSystemID}/${gameModeID}`,await this.database.getTargetCollection(gameSystemID,gameModeID) )
-        return this.resolveID(id,this.targets.get(`${gameSystemID}/${gameModeID}`),lang,"Target");
-    }
-    async resolveGameDifficultyID(gameSystemID:string,gameModeID:string,id:string,lang:LanguageInApplication){
-        this.checkInitalized();
-        if (!this.difficulties.has(`${gameSystemID}/${gameModeID}`)) this.difficulties.set(`${gameSystemID}/${gameModeID}`,await this.database.getGameDifficultyCollection(gameSystemID,gameModeID) )
-        return this.resolveID(id,this.difficulties.get(`${gameSystemID}/${gameModeID}`),lang,"GameDifficulty");
-    }
-
 
     async convertRecordsIntoRecordGroupResolved(records: IRecord[],
         info: { groupName: string; numberOfRecords: number; numberOfRunners: number; lang: LanguageInApplication; }): Promise<IRecordGroupResolved> {
-        this.checkInitalized();
         const copy = records.concat();
         return {
             groupName: info.groupName,
@@ -108,10 +83,8 @@ export class ControllerOfTableForResolvingID{
 
 
     async convertIRecordIntoIRecordInShortWithName(record: IRecord, lang: LanguageInApplication): Promise<IRecordInShortResolved> {
-        if (!this.initalized) throw new Error(this.initErorMsg)
         const gr = record.regulation; //#README
         const gse = gr.gameSystemEnvironment; //#README
-        this.checkInitalized();
         return {
             regulation: {
                 gameSystemEnvironment: {
@@ -136,7 +109,6 @@ export class ControllerOfTableForResolvingID{
 
 
     async convertRecordIntoRecordResolved(record:IRecord,lang:LanguageInApplication):Promise<IRecordResolved>{
-        this.checkInitalized();
         const rr = record.regulation;
         const rrg = record.regulation.gameSystemEnvironment;
         return {
@@ -160,7 +132,7 @@ export class ControllerOfTableForResolvingID{
             runnerID: record.runnerID,
             runnerName: await this.resolveRunnerID(record.runnerID,lang),
             tagID: record.tagID,
-            tagName: await Promise.all(record.tagID.map((element) => this.resolveTagID(rrg.gameSystemID,rrg.gameModeID,element,lang))),
+            tagName: await Promise.all(record.tagID.map((element) => this.resolveTagID(rrg.gameSystemID,element,lang))),
             note: record.note,
             link: record.link
         }

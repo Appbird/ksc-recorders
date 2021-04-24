e4bd074d972de2803a09976dc9b6a9021f5ae3c0
Appbird <project.appbird@gmail.com>
2021-04-24 21:03:53 +0900

    [add] クライアント側にてクラスLoginAdministratorを実装。

--- src/ts/client/Administrator/LoginAdministrator.ts
+++ src/ts/client/Administrator/LoginAdministrator.ts
@@ -1,23 +1,46 @@
+import App from "../App";
+import firebase from "../firebase"
 export class LoginAdministrator{
     private iconAboutloginUser:HTMLImageElement;
-    constructor(){
+    private app:App;
+    private provider:firebase.auth.GoogleAuthProvider;
+    constructor(app:App){
         const result = document.querySelector("header>img.c-userIcon")
         if (result === null) throw new Error("予期せぬエラーです。")
-        this.iconAboutloginUser = result as HTMLImageElement;   
-    }
-    login(){
+        this.iconAboutloginUser = result as HTMLImageElement;
+        this.provider = new firebase.auth.GoogleAuthProvider();
+        this.app = app;
+        firebase.auth().useDeviceLanguage();
 
     }
-    logout(){
-
+    /**@throw */
+    async login(){
+        try{
+            await firebase.auth().signInWithPopup(this.provider);
+        }catch(error){
+            this.app.errorCatcher(error);
+        }
     }
-    getName(){
-
+    /**@throw */
+    async logout(){
+        await firebase.auth().signOut()
     }
-    getIconPicture(){
-
+    /**@throw */
+    get loginUserName(){
+        const user = firebase.auth().currentUser;
+        if ( user === null ) throw new Error("ログインしていません。")
+        return user.displayName;
     }
-    getIDToken(){
-
+    /**@throw */
+    get loginUserIconPicture(){
+        const user = firebase.auth().currentUser;
+        if ( user === null ) throw new Error("ログインしていません。")
+        return firebase.auth().currentUser?.photoURL;
+    }
+    /**@throw */
+    get IDToken(){
+        const user = firebase.auth().currentUser;
+        if ( user === null ) throw new Error("ログインしていません。")
+        return user.getIdToken(true);
     }
 }

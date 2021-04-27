f3a5053267900cff1c628d1719120939b1b41f74
Appbird <project.appbird@gmail.com>
2021-04-28 00:02:23 +0900

    [add] IcoonMono様のアイコンをリポジトリに追加
    [add] ログイン時の表示などを微調整

--- src/ts/client/Administrator/HeaderController.ts
+++ src/ts/client/Administrator/HeaderController.ts
@@ -3,10 +3,27 @@ export class HeaderController{
     private element:Element;
     private main:Element;
     private sub:Element;
+    private icon:HTMLImageElement;
+    private logInState:HTMLElement;
     constructor(){
         this.element = checkIsNull(document.getElementById("headerDisplay"),`id="headerDisplay"となる要素がありませんでした。`)
         this.main = checkIsNull(this.element.firstElementChild,`id="headerDisplay"となる要素の子要素がありませんでした。`);
         this.sub = checkIsNull(this.element.lastElementChild,`id="headerDisplay"となる要素の子要素がありませんでした。`);
+
+        const logIn = checkIsNull(document.getElementById("userIcon"),`id=userIconとなる要素がありませんでした。`)
+        this.icon = checkIsNull(logIn.lastElementChild,`id=userIconとなる要素の子要素がありませんでした。`) as HTMLImageElement;
+        this.logInState = checkIsNull(logIn.firstElementChild,`id=userIconとなる要素の子要素がありませんでした。`) as HTMLImageElement;
+    }
+    changeUserIcon(userName:string,href:string|null|undefined){
+        if (href === null || href===undefined){
+            this.icon.src = "./icon/icooonMono/person.svg"; return;
+        }
+        this.logInState.innerHTML = `Log in:<strong>${userName}</strong>`
+        this.icon.src = href;
+    }
+    deleteUserIcon(){
+        this.logInState.innerText = `Log out`
+        this.icon.src = "./icon/icooonMono/person.svg";
     }
     async changeHeaderRightLeft(str:string,sub:string){
         await new Promise((resolve) => setTimeout(()=>resolve(undefined),500));
--- src/ts/client/index.ts
+++ src/ts/client/index.ts
@@ -2,7 +2,5 @@ import App from "./App";
 const articleDOM =  document.getElementById("article")
 if (articleDOM === null) throw new Error("idがarticleである要素を見つけられませんでした。")
 const app = new App(articleDOM,"Japanese");
-(async () => {
-    await app.init();
-    app.transition("mainMenu",null);
-})();
+
+app.init().then(() => app.transition("mainMenu",null));
